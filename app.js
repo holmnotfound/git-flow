f4618b94800bba534a4f7eb17ec3103e69500eaa
AdrianRydin <adrian.rydin@gmail.com>
2025-01-31 10:30:19 +0100

    changed app.js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 import apiHandler from "./apiHandler.js";
 import pagination from "./pagination.js";
 import characters from "./characters.js";
+
+pageSetup();
+
+function pageSetup() {
+  fetchCharacters();
+  paginationSetup();
+
+  document
+    .querySelector("#searchInput")
+    .addEventListener("input", updateAutoCompleteList);
+}
