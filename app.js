870cc4f56e789da82d278b9df962175f072098f7
AdrianRydin <adrian.rydin@gmail.com>
2025-01-31 10:42:17 +0100

    added js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 import apiHandler from "./apiHandler.js";
 import pagination from "./pagination.js";
 import characters from "./characters.js";
+
+async function renderPersonDetails(url) {}
+
+async function renderHomeworldDetails(url) {}
+
+function updateAutoCompleteList(event) {}
