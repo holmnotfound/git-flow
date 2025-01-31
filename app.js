00a70a083d7303527e5cf4638329a46cb6b65ec3
AdrianRydin <adrian.rydin@gmail.com>
2025-01-31 10:37:47 +0100

    added js

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 import apiHandler from "./apiHandler.js";
 import pagination from "./pagination.js";
 import characters from "./characters.js";
+
+function paginationSetup() {
+  const prevRef = document.querySelector("#prevPageBtn");
+  const nextRef = document.querySelector("#nextPageBtn");
+
+  prevRef.addEventListener("click", () => {
+    pagination.previousPage();
+    renderCharacters();
+  });
+
+  nextRef.addEventListener("click", () => {
+    pagination.nextPage();
+    renderCharacters();
+  });
+}
