d8c3892a2ea60b19c545586e50362f7d62669f98
AdrianRydin <adrian.rydin@gmail.com>
2025-01-31 10:31:26 +0100

    added app.js

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 import apiHandler from "./apiHandler.js";
 import pagination from "./pagination.js";
 import characters from "./characters.js";
+
+async function fetchCharacters() {
+  let nextUrl = "https://swapi.dev/api/people/";
+
+  while (nextUrl) {
+    const data = await apiHandler.fetchData(nextUrl);
+
+    data.results.forEach((character) => {
+      characters.pushCharacter(character);
+    });
+
+    nextUrl = data.next;
+  }
+
+  pagination.setNmbrOfPosts(characters.getCharacters().length);
+  renderCharacters();
+}
